c8d09e7da47307210862ee004fb96363d6fb3556
Glair Narra <glair.narra@tooltwist.com>
2021-04-18 11:13:14 +0800

    [chore] - Additional setup :art:
    1. Added assets directory for native assets (fonts) that needs linking
    2. Added the "react-native-config.js" file for linking native assets
    3. Added the "babel-module-resolver" dependency which will be used
    for using absolute paths on the codebase

---
+++ react-native-config.js
@@ -0,0 +1,13 @@
+/**
+ * This file is used in order to link the assets of the app
+ * to the native platform (ios/android)
+ * run "npx react-native link" to link assets
+ */
+
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: ['./assets'],
+};
