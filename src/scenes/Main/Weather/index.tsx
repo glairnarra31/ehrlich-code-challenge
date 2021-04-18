038b52ec6ce9436e906c7525c5b29e6619c503fa
Glair Narra <glair.narra@tooltwist.com>
2021-04-18 20:36:36 +0800

    [feat] - Weather Scene :sparkles:
    1. Implement orientation detection on UI
    2. Implement table component for the layout
    3. Implement table UI with mocked data

--- src/scenes/Main/Weather/index.tsx
+++ src/scenes/Main/Weather/index.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View, Dimensions} from 'react-native';
+import {Table} from '@/components'
 
 export default () => {
+  const simpleData = [
+    ['Date (mm/dd/yy)', 'Temperature (F)'],
+    ['09/01/2020', '75'],
+  ];
+
+  const expandedData = [
+    ['Date\n(mm/dd/yy)', 'Temperature (F)', 'Description', 'Main', 'Pressure', 'Humidity'],
+    ['09/01/2020', '75', 'Sky is clear', 'Clear', '1023.68', '100'],
+  ];
+
+  const [orientation, setOrientation] = React.useState("PORTRAIT");
+
+  React.useEffect(() => {
+    Dimensions.addEventListener('change', ({window:{width,height}})=>{
+        setOrientation(width < height ? 'PORTRAIT' : 'LANDSCAPE')
+    })
+  }, []);
+
   return (
       <View style={styles.container}>
-        <Text>Weather!</Text>
+        <Table data={orientation === 'PORTRAIT' ? simpleData : expandedData} />
       </View>
   );
 };
@@ -12,7 +31,6 @@ export default () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+    padding: 20,
+  }
 });
